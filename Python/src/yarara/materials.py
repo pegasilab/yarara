7d2458982c4112b7042913d9f722f2c5a9f35838
Michael Cretignier <44569244+MichaelCretignier@users.noreply.github.com>
2022-11-28 14:33:36 +0100

    Update materials.py
    Add other static product typedict

--- Python/src/yarara/materials.py
+++ Python/src/yarara/materials.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from numpy.typing import NDArray
 from typing_extensions import TypedDict
 
@@ -14,3 +15,33 @@ class Contam_HARPN(TypedDict):
     contam_2: NDArray[np.bool_]
     #: Tells which wavelenghts are contaminated (True = contaminated)
     contam_backup: NDArray[np.bool_]
+
+    
+class Telluric_spectrum(TypedDict):
+    #: Wavelengths in angstroms
+    wave: NDArray[np.float64]
+    #: Telluric spectrum in normalised flux units
+    flux_norm: NDArray[np.float64]
+
+
+class Table_stellar_model(TypedDict):
+    #: Table of Mass/Temperature/Radius from Gray+09 for dwarfs stars
+    V: pd.DataFrame 
+    #: Table of Mass/Temperature/Radius from Gray+09 for evolved stars 
+    IV: pd.DataFrame
+
+
+class Ghost_HARPN(TypedDict):
+    #: JDB time of the reference wavelength solution 
+    jdb: float 
+    #: Not used anymore 
+    berv: float
+    #: 2D matrix of the reference wavelength solution 
+    wave: NDArray[np.float32]
+    #: 2D matrix of the stitching contamination (fiber A)
+    stitching: NDArray
+    #: 2D matrix of the ghost contamination (fiber A)
+    ghost_a: NDArray
+    #: 2D matrix of the ghost contamination (fiber B)
+    ghost_b: NDArray
+
